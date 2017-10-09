250e124b7948500129972b87e7763ba4cf10cfe9
yash Shah <yash_shah89@yahoo.com>
2017-10-09 12:26:58 +0530

    task01: initial commit
    Signed-off-by: yash Shah <yash_shah89@yahoo.com>

---
+++ task01/hello.c
@@ -0,0 +1,21 @@
+#include<linux/kernel.h>
+#include<linux/init.h>
+#include<linux/module.h>
+
+static int __init hello(void)
+{
+	printk(KERN_DEBUG "Hello World!\n");
+	return 0;
+}
+
+static void __exit bye(void)
+{
+	printk(KERN_DEBUG "unloding hello module\n");
+}
+
+module_init(hello);
+module_exit(bye);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Yash Shah");
+MODULE_DESCRIPTION("hello world module");
